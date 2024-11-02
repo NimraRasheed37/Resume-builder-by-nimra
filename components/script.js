ea8f60a084645d6a6a544b97666c9b9511bbbd13
NimraRasheed37 <binteshaheen.na@gmail.com>
2024-11-02 20:56:42 +0500

    1st commit

---
+++ components/script.js
@@ -0,0 +1,16 @@
+const navBar = document.getElementById('nav-bar');
+const sideBar = document.getElementById('side-bar');
+const sideBarLinks = document.querySelectorAll("#side-bar .side-bar-links a");
+
+
+//open sidebar when clicked on navBar 
+navBar.addEventListener('click', () => {
+    sideBar.classList.toggle("active");
+});
+
+//close sidebar when any link is clicked
+sideBarLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        sideBar.classList.remove("active");
+    });
+});
